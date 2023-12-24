87cd0987e0eaa627d92bb3918e6c07a526c4ffd0
xuyaomin <731945017@qq.com>
2023-12-24 16:20:37 +0800

    输出按照大小排序

--- file_search.py
+++ file_search.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 # 文件夹大小, 子节转MB, GB
 def format_folder_size(folder_size):
@@ -7,6 +8,20 @@ def format_folder_size(folder_size):
     if gb > 1:
         return "{:.0f}GB".format(gb)
     return "{:.0f}MB".format(mb)
+
+def print_order_results(results):
+    data = ""
+    for s in results:
+        data += s + "\r\n"
+    # 提取文件夹名称和大小信息并存储为元组列表
+    folder_info_list = re.findall(r"文件夹\s+([^的]+)\s+的大小为:\s+(\d+\.?\d*)\s*(\w+)", data)
+    # 根据文件夹大小排序
+    sorted_folders = sorted(folder_info_list, key=lambda x: float(x[1]) * (1024 if x[2] == 'GB' else 1) if x[2] != 'MB' else float(x[1]), reverse=True)
+    # 打印排序结果
+    for name, size, unit in sorted_folders:
+        print(f"文件夹 {name} 的大小为: {size} {unit}")
+
+
 
 def get_folder_size(folder_path):
     total_size = 0
@@ -33,7 +48,8 @@ def print_folder_sizes(base_path="."):
                 continue
             if entry.is_dir():
                 folder_size = get_folder_size(entry.path)
-                print(f"文件夹 {entry.name} 的大小为: {format_folder_size(folder_size)}")
+                # print(f"文件夹 {entry.name} 的大小为: {format_folder_size(folder_size)}")
+                results.append(f"文件夹 {entry.name} 的大小为: {format_folder_size(folder_size)}")
                 total_size += folder_size
     return total_size
 
@@ -47,7 +63,8 @@ def main():
 
     if os.path.exists(folder_path):
         folder_size = print_folder_sizes(folder_path)
-        print(f"文件夹 {folder_path} 的大小为: {format_folder_size(folder_size)}")
+        # print(f"文件夹 {folder_path} 的大小为: {format_folder_size(folder_size)}")
+        results.append(f"文件夹 {folder_path} 的大小为: {format_folder_size(folder_size)}")
     else:
         print("指定的文件夹路径不存在")
 
@@ -55,4 +72,9 @@ def main():
 if __name__ == "__main__":
     # 白名单, 跳过遍历
     white_names = ["$RECYCLE.BIN", "MSOCache", "System Volume Information"]
+    # 结果
+    results = []
+    # 开始遍历
     main()
+    # 输出结果
+    print_order_results(results)
